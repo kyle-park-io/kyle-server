225639d10a18e2c2f7d4b96b8e3582c6e3cd47bf
kyle-park-io <andy3638@naver.com>
2023-12-02 03:09:31 +0900

    feat: implement checking existed folder function

--- packages/md-to-html/index.ts
+++ packages/md-to-html/index.ts
@@ -7,6 +7,14 @@ export async function convert(): Promise<void> {
     const marked = new Marked();
     const markdownDirPath = path.resolve('md');
     const htmlOutputPath = path.resolve('html');
+
+    if (!fs.existsSync(htmlOutputPath)) {
+      fs.mkdirSync(htmlOutputPath, { recursive: true });
+      console.log(`Folder created at: ${htmlOutputPath}`);
+    } else {
+      console.log(`Folder already exists at: ${htmlOutputPath}`);
+    }
+
     const markdown = fs.readdirSync(markdownDirPath, 'utf8');
     for (let i = 0; i < markdown.length; i++) {
       const file = fs.readFileSync(markdownDirPath + '/' + markdown[i], 'utf8');
