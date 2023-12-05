6e30c726c29f534a09e2a54e627f7058cfa6f9a2
kyle-park-io <andy3638@naver.com>
2023-12-05 09:50:14 +0900

    feat: modify grid size to w-full

--- packages/blog-frontend/src/App.tsx
+++ packages/blog-frontend/src/App.tsx
@@ -13,41 +13,44 @@ const App: Component = (): JSX.Element => {
 
   return (
     <>
-      <div>
+      <div class="flex-grow flex flex-col">
         <div>
           <h1 class="underline">Hello world!</h1>
         </div>
-        <div class="grid grid-cols-3 gap-4">
-          <div class="bg-blue-200 p-4">
-            <a href={`${url}/blog`}>블로그</a>
+        <div class="flex-grow center-flex">
+          <div class="w-full grid grid-cols-3 gap-2">
+            <div class="w-full h-[100px] bg-blue-200 center-flex">
+              <a href={`${url}/blog`}>블로그</a>
+            </div>
+            <div class="w-full h-[100px] bg-blue-200 center-flex">CAREER</div>
+            <div class="w-full h-[100px] bg-blue-200 center-flex">DEX</div>
+            <div class="w-full h-[100px] bg-blue-200 center-flex">
+              <a
+                href="https://www.linkedin.com/in/kyle-park-io"
+                target=""
+                rel="noopener noreferrer"
+              >
+                LINKEDIN
+              </a>
+            </div>
+            <div class="w-full h-[100px] bg-blue-200 center-flex">
+              <a
+                href="https://github.com/kyle-park-io"
+                target=""
+                rel="noopener noreferrer"
+              >
+                GITHUB
+              </a>
+            </div>
+            <div class="w-full h-[100px] bg-blue-200 center-flex">
+              ANONYMOUS<br></br>CHAT
+            </div>
           </div>
-          <div class="bg-blue-200 p-4">덱스</div>
-          <div class="bg-blue-200 p-4">
-            <a
-              href="https://www.linkedin.com/in/kyle-park-io"
-              target=""
-              rel="noopener noreferrer"
-            >
-              링크드인
-            </a>
-          </div>
-          <div class="bg-blue-200 p-4">
-            <a
-              href="https://github.com/kyle-park-io"
-              target=""
-              rel="noopener noreferrer"
-            >
-              깃허브
-            </a>
-          </div>
-          <div class="bg-blue-200 p-4">방명록</div>
-          <div class="bg-blue-200 p-4">공지사항</div>
+          {/* <p class="read-the-docs">
+            Click on the Vite and Solid logos to learn more
+          </p> */}
         </div>
       </div>
-
-      {/* <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p> */}
     </>
   );
 };
