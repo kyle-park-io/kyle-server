3be4e4af8d279bbe732ce2dd7f15365d7835d5e7
kyle-park-io <andy3638@naver.com>
2024-06-21 14:19:48 +0900

    feat: add copy functions

--- packages/ingress-proxy/utils/os.go
+++ packages/ingress-proxy/utils/os.go
@@ -1,6 +1,10 @@
 package utils
 
-import "os"
+import (
+	"io"
+	"os"
+	"path/filepath"
+)
 
 func FileExists(path string) bool {
 	_, err := os.Stat(path)
@@ -8,4 +12,51 @@ func FileExists(path string) bool {
 		return false
 	}
 	return err == nil
+}
+
+func CopyFile(src, dst string) error {
+	sourceFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	destinationFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destinationFile.Close()
+
+	_, err = io.Copy(destinationFile, sourceFile)
+	if err != nil {
+		return err
+	}
+
+	err = destinationFile.Sync()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func CopyDir(src string, dst string) error {
+	return filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		relativePath, err := filepath.Rel(src, path)
+		if err != nil {
+			return err
+		}
+
+		destinationPath := filepath.Join(dst, relativePath)
+
+		if info.IsDir() {
+			return os.MkdirAll(destinationPath, info.Mode())
+		}
+
+		return CopyFile(path, destinationPath)
+	})
 }
