cdf39642577094dddb43d0e3dbbbb6b2b0c5616e
kyle-park-io <andy3638@naver.com>
2024-06-21 14:17:39 +0900

    refactor: move cors, middleware to separate package

---
+++ packages/ingress-proxy/middleware/middleware.go
@@ -0,0 +1,23 @@
+package middleware
+
+import (
+	"ingress-proxy/cors"
+	"net/http"
+)
+
+func Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// CORS 헤더 설정
+		cors.EnableCors(w)
+
+		// Preflight 요청에 대한 처리
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		// Preflight 요청이 아닌 경우 다음 핸들러로 요청을 전달
+		next.ServeHTTP(w, r)
+	})
+}
