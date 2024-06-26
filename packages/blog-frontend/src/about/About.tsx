9cdfd85bfc47c622c79cf57c599721328b1d69f6
kyle-park-io <andy3638@naver.com>
2024-06-26 16:33:57 +0900

    feat: update content on profile and about pages

--- packages/blog-frontend/src/about/About.tsx
+++ packages/blog-frontend/src/about/About.tsx
@@ -1,5 +1,7 @@
 import { type Component, type JSX } from 'solid-js';
-import { Container, Row, Col } from 'solid-bootstrap';
+import { Container, Row, Col, Image } from 'solid-bootstrap';
+// image
+import Cat from '/cat-bg.jpg?url';
 
 const About: Component = (): JSX.Element => {
   return (
@@ -11,10 +13,95 @@ const About: Component = (): JSX.Element => {
           </Col>
         </Row>
         <Row>
-          <Col md={8}>
-            <span>body</span>
+          <Col md={7} xs={6}>
+            <ol>
+              <li>
+                <p>
+                  본 사이트는 오케스트레이션 연습 및 개인 프로젝트들을
+                  업로드하기 위해 구축되었습니다.
+                </p>
+              </li>
+              <li>
+                <p>
+                  관련된 코드, 개발 문서 등은 얼마든지 참고 및 공유가 가능하니,
+                  이를 통해 지속적인 추가 개발이 이루어지기를 희망합니다.
+                </p>
+              </li>
+              <li>
+                <p>본 사이트에 적용된 기술 및 개발 언어는 다음과 같습니다.</p>
+                <ul>
+                  <li>GKE (GCP)</li>
+                  <li>Kubenetes</li>
+                  <li>Docker</li>
+                  <li>
+                    Go
+                    <ul>
+                      <li>
+                        Backend
+                        <ul>
+                          <li>net/http</li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    Typescript (NodeJS)
+                    <ul>
+                      <li>
+                        Backend
+                        <ul>
+                          <li>ExpressJS</li>
+                        </ul>
+                      </li>
+                      <li>
+                        Frontend
+                        <ul>
+                          <li>SolidJS</li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+                <br />
+              </li>
+              <li>
+                <p>
+                  개인 프로젝트들의 기술 구성은 각 프로젝트 About 을 참고하시기
+                  바랍니다.
+                </p>
+              </li>
+              <li>
+                <p>
+                  기술에 대한 자유로운 의견 공유는 언제나 환영합니다. 다음의
+                  방법을 활용해주세요!
+                </p>
+                <ul>
+                  <li>E-mail : andy3638@naver.com</li>
+                  <li>
+                    LinkedIn :{' '}
+                    <a href="https://www.linkedin.com/in/kyle-park-io">
+                      https://www.linkedin.com/in/kyle-park-io
+                    </a>
+                  </li>
+                  <li>
+                    Github :{' '}
+                    <a href="https://github.com/kyle-park-io">
+                      https://github.com/kyle-park-io
+                    </a>
+                  </li>
+                  <li>
+                    OpenChat :{' '}
+                    <a href="https://open.kakao.com/o/s11ostzg">
+                      https://open.kakao.com/o/s11ostzg
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ol>
           </Col>
-          <Col md={4}>image</Col>
+          <Col md={5} xs={6}>
+            <Image src={Cat} fluid></Image>
+          </Col>
         </Row>
       </Container>
     </>
--- packages/blog-frontend/src/profile/Profile.tsx
+++ packages/blog-frontend/src/profile/Profile.tsx
@@ -1,5 +1,7 @@
 import { type Component, type JSX } from 'solid-js';
-import { Container, Row, Col } from 'solid-bootstrap';
+import { Container, Row, Col, Image } from 'solid-bootstrap';
+// image
+import Kyle from '/kyle-bg.webp?url';
 
 const Profile: Component = (): JSX.Element => {
   return (
@@ -11,10 +13,68 @@ const Profile: Component = (): JSX.Element => {
           </Col>
         </Row>
         <Row>
-          <Col md={8}>
-            <span>body</span>
+          <Col md={7} xs={6}>
+            <h3 id="-">학력</h3>
+            <p>경희대학교 공과대학 기계공학과 졸업 (2014.03 ~ 2022.08)</p>
+            <h3 id="-">경력</h3>
+            <ol>
+              <li>
+                ㈜ 미디움
+                <ol>
+                  <li>기간 : 2022.09 ~ 2024.02 (18 개월)</li>
+                  <li>직급 : 코어팀 · 매니저 2년차</li>
+                  <li>업무 : 블록체인</li>
+                </ol>
+              </li>
+              <li>
+                ㈜ 비에프랩스
+                <ol>
+                  <li>기간 : 2022.08 ~ 2022.09 (2 개월)</li>
+                  <li>직급 : 개발팀 · 사원 1년차</li>
+                  <li>업무 : 블록체인</li>
+                </ol>
+              </li>
+            </ol>
+            <h3 id="-">경력 기술서</h3>
+            <ol>
+              <li>000</li>
+            </ol>
+            <h3 id="-">자기소개서</h3>
+            <p>
+              <a href="https://www.notion.so/f3ea582e4db84ad3b256b4f153349d02?pvs=21">
+                https://kyle-park.notion.site/f3ea582e4db84ad3b256b4f153349d02?pvs=25
+              </a>
+            </p>
+            <h3 id="-">포트폴리오</h3>
+            <ol>
+              <li>
+                개인프로젝트
+                <br />
+                <a href="https://www.notion.so/Portfolio-a69711e7a8484ec08821c84199900e37?pvs=21">
+                  https://kyle-park.notion.site/Portfolio-a69711e7a8484ec08821c84199900e37?pvs=4
+                </a>
+              </li>
+              <li>
+                기술 문서
+                <br />
+                <a href="https://www.notion.so/c92ee6d25f1f48fe8fa54ef5fa79790c?pvs=21">
+                  https://kyle-park.notion.site/c92ee6d25f1f48fe8fa54ef5fa79790c?pvs=74
+                </a>
+              </li>
+            </ol>
+            <h3 id="-">자격/어학/수상</h3>
+            <p>2021 공조냉동기계기사</p>
+            <p>2020 일반기계기사</p>
+            <p>2021 TOEIC Speaking Test / 150 (intermediate High)</p>
+            <p>2019 컴퓨터활용능력 1급</p>
+            <p>2005 워드프로세서 1급</p>
+            <p>2015 1종 보통운전면허</p>
+            <h3 id="mbti">MBTI</h3>
+            <p>ENTJ</p>
           </Col>
-          <Col md={4}>image</Col>
+          <Col md={5} xs={6}>
+            <Image src={Kyle} fluid></Image>
+          </Col>
         </Row>
       </Container>
     </>
