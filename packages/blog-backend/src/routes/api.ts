6f9f08b9e980282e009c8ca80a36bd88d3477d67
kyle-park-io <andy3638@naver.com>
2024-07-08 19:06:30 +0900

    feat: add api to fetch blog informations

--- packages/blog-backend/src/routes/api.ts
+++ packages/blog-backend/src/routes/api.ts
@@ -1,11 +1,22 @@
 import { type Request, type Response } from 'express';
 import express from 'express';
-import { blogList, blogDetail, blogDownload } from '../services/blog';
+import {
+  blogListNumber,
+  blogList,
+  blogDetail,
+  blogDownload,
+} from '../services/blog';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { update } from '../utils/md';
 
 const apiRouter = express.Router();
 
+apiRouter.get('/blog/number', (req: Request, res: Response) => {
+  void blogListNumber(req, res);
+});
+apiRouter.get('/blog/sorted-by-date/top-10', (req: Request, res: Response) => {
+  void blogListNumber(req, res);
+});
 apiRouter.get('/blog', (req: Request, res: Response) => {
   void blogList(req, res);
 });
--- packages/blog-backend/src/services/blog.ts
+++ packages/blog-backend/src/services/blog.ts
@@ -2,6 +2,48 @@ import { type Request, type Response } from 'express';
 import fs from 'fs';
 import * as path from 'path';
 
+export const blogListNumber = async (
+  req: Request,
+  res: Response,
+): Promise<any> => {
+  try {
+    const htmlOutputPath = path.join('/usr/src/app/html');
+    const list = fs.readdirSync(htmlOutputPath, 'utf8');
+    res.json(list.length);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err.message);
+      res.status(500).send(err.message);
+    } else {
+      console.error('An unexpected error occurred:', err);
+      res.status(500).send(new Error('An unexpected error occurred'));
+    }
+  }
+};
+
+export const blogGetTop10SortedByDate = async (
+  req: Request,
+  res: Response,
+): Promise<any> => {
+  try {
+    const htmlOutputPath = path.join('/usr/src/app/html');
+    const list = fs.readdirSync(htmlOutputPath, 'utf8');
+    const result: string[] = [];
+    for (let i = 0; i < list.length; i++) {
+      result.push(list[i].split('.')[0]);
+    }
+    res.json(result);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err.message);
+      res.status(500).send(err.message);
+    } else {
+      console.error('An unexpected error occurred:', err);
+      res.status(500).send(new Error('An unexpected error occurred'));
+    }
+  }
+};
+
 export const blogList = async (req: Request, res: Response): Promise<any> => {
   try {
     const htmlOutputPath = path.join('/usr/src/app/html');
