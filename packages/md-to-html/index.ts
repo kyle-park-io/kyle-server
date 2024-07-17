05a7ac520842f98a8de911010196dfe80e6a8b36
kyle-park-io <andy3638@naver.com>
2024-07-17 19:14:41 +0900

    feat: add alignment function to md-to-html3 package

--- packages/md-to-html/index.ts
+++ packages/md-to-html/index.ts
@@ -16,6 +16,7 @@ export async function convert(): Promise<void> {
     }
 
     const markdown = fs.readdirSync(markdownDirPath, 'utf8');
+    sortedByModifiedTime(markdown);
     for (let i = 0; i < markdown.length; i++) {
       const file = fs.readFileSync(markdownDirPath + '/' + markdown[i], 'utf8');
       const html = await marked.parse(file);
@@ -24,6 +25,39 @@ export async function convert(): Promise<void> {
         html,
       );
     }
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+export function sortedByModifiedTime(files: string[]): void {
+  try {
+    const markdownDirPath = path.resolve('md');
+    const sortDirPath = path.resolve('sort');
+    if (!fs.existsSync(sortDirPath)) {
+      fs.mkdirSync(sortDirPath, { recursive: true });
+      console.log(`Folder created at: ${sortDirPath}`);
+    } else {
+      console.log(`Folder already exists at: ${sortDirPath}`);
+    }
+
+    const fileInfos = files.map((file) => {
+      const filePath = path.join(markdownDirPath, file);
+      const stats = fs.statSync(filePath);
+      return {
+        file: file,
+        mtime: stats.mtime.getTime(),
+      };
+    });
+
+    fileInfos.sort((a, b) => b.mtime - a.mtime);
+    console.log(fileInfos);
+    const sortedFiles = fileInfos.map((fileInfo) => fileInfo.file);
+    fs.writeFileSync(
+      sortDirPath + '/sorted-files.txt',
+      JSON.stringify(sortedFiles),
+    );
   } catch (err) {
     console.log(err);
     throw err;
