234737714ebc435e52a2a60fdee296b28ac21d45
kyle-park-io <andy3638@naver.com>
2024-08-15 18:21:19 +0900

    refactor: apply updated pod and service names to server

--- packages/blog-backend/push2gke_artifact.sh
+++ packages/blog-backend/push2gke_artifact.sh
@@ -3,9 +3,14 @@
 PROJECT_ID=kyle-server-402706
 REPOSITORY=kyle-registry
 LOCATION=me-west1
-IMAGE=site-node
+IMAGE=site-app-server
 TAG=0.0.1
 TAG_LATEST=latest
+
+# dir
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+DOCKERFILE_PATH=$SCRIPT_DIR/Dockerfile
+CONTEXT_PATH=$SCRIPT_DIR
 
 set -e
 
@@ -18,16 +23,16 @@ gcloud artifacts docker tags delete $LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSI
 DIGEST=$(gcloud artifacts docker images list $LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY --filter="PACKAGE: $IMAGE" --format="get(DIGEST)" --limit=1)
 gcloud artifacts docker images delete $LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE@$DIGEST --quiet || true
 
-IMAGE_TAG=me-west1-docker.pkg.dev/kyle-server-402706/kyle-registry/site-node:0.0.1
+# IMAGE_TAG=me-west1-docker.pkg.dev/kyle-server-402706/kyle-registry/site-app-server:0.0.1
+IMAGE_TAG=$LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE:$TAG
 # --no-cache
-docker buildx build --no-cache --platform linux/amd64 --build-arg=PROGRAM_VER=0.0.1 -t $IMAGE_TAG .
+docker buildx build --no-cache --build-arg=PROGRAM_VER=0.0.1 --load -t $IMAGE_TAG -f $DOCKERFILE_PATH $CONTEXT_PATH
+# docker buildx build --no-cache --platform linux/amd64 --build-arg=PROGRAM_VER=0.0.1 --push -t $IMAGE_TAG -f $DOCKERFILE_PATH $CONTEXT_PATH
 docker push $IMAGE_TAG
 
-IMAGE_TAG_LATEST=me-west1-docker.pkg.dev/kyle-server-402706/kyle-registry/site-node:latest
+IMAGE_TAG_LATEST=$LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE:$TAG_LATEST
 docker tag $IMAGE_TAG $IMAGE_TAG_LATEST
 docker push $IMAGE_TAG_LATEST
 
 # # git tag -a $1 -m "add tag for $1"
 # # git push origin main --tags
-
-# # gcloud container clusters get-credentials kyle-ingress-standard-cluster-1 --zone me-west1-a --project kyle-server-402706
--- packages/ingress-proxy/push2gke_artifact.sh
+++ packages/ingress-proxy/push2gke_artifact.sh
@@ -3,9 +3,14 @@
 PROJECT_ID=kyle-server-402706
 REPOSITORY=kyle-registry
 LOCATION=me-west1
-IMAGE=ingress-go
+IMAGE=ingress-proxy-app-server
 TAG=0.0.1
 TAG_LATEST=latest
+
+# dir
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+DOCKERFILE_PATH=$SCRIPT_DIR/Dockerfile
+CONTEXT_PATH=$SCRIPT_DIR
 
 set -e
 
@@ -18,12 +23,14 @@ gcloud artifacts docker tags delete $LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSI
 DIGEST=$(gcloud artifacts docker images list $LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY --filter="PACKAGE: $IMAGE" --format="get(DIGEST)" --limit=1)
 gcloud artifacts docker images delete $LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE@$DIGEST --quiet || true
 
-IMAGE_TAG=me-west1-docker.pkg.dev/kyle-server-402706/kyle-registry/ingress-go:0.0.1
+# IMAGE_TAG=me-west1-docker.pkg.dev/kyle-server-402706/kyle-registry/ingress-proxy-app-server:0.0.1
+IMAGE_TAG=$LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE:$TAG
 # --no-cache
-docker buildx build --no-cache --platform linux/amd64 --build-arg=PROGRAM_VER=0.0.1 -t $IMAGE_TAG .
+docker buildx build --no-cache --build-arg=PROGRAM_VER=0.0.1 --load -t $IMAGE_TAG -f $DOCKERFILE_PATH $CONTEXT_PATH
+# docker buildx build --no-cache --platform linux/amd64 --build-arg=PROGRAM_VER=0.0.1 --push -t $IMAGE_TAG -f $DOCKERFILE_PATH $CONTEXT_PATH
 docker push $IMAGE_TAG
 
-IMAGE_TAG_LATEST=me-west1-docker.pkg.dev/kyle-server-402706/kyle-registry/ingress-go:latest
+IMAGE_TAG_LATEST=$LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE:$TAG_LATEST
 docker tag $IMAGE_TAG $IMAGE_TAG_LATEST
 docker push $IMAGE_TAG_LATEST
 
